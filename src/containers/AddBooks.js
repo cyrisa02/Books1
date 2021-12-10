d874f98a92ae9bab2ad1b97af73056b6a462dcbc
Cyril <cyril.gourdon.02@gmail.com>
2021-12-10 09:28:58 +0100

    démarrage et validation du formulaire

---
+++ src/containers/AddBooks.js
@@ -0,0 +1,77 @@
+import React, {useState} from 'react'
+
+  const AddBooks = () => {
+    const initialState = {
+      title: '',
+      author:''
+    }
+
+    const [newData, setnewData] = useState(initialState);
+    
+
+const handleSubmit = e => {
+  e.preventDefault();
+   console.log(newData);
+}
+
+    return (
+      <main role="main">
+        <div className="jumbotron jumbotron-fluid">
+          <div className="container text-center">
+            <h1 className="display-4">BOOKS</h1>
+            <p>Ajouter un livre à votre Bibliothèque</p>
+
+            <form className="form-inline justify-content-center" onSubmit={handleSubmit}>
+              <div className="form-group p-3">
+                <input
+                  value={newData.title}
+                  type="text"
+                  className="form-control"
+                  placeholder="Titre"
+                  required
+                  onChange={(e) =>
+                    setnewData({ ...newData, title: e.target.value })
+                  }
+                />
+              </div>
+              <div className="form-group p-3">
+                <input
+                  value={newData.author}
+                  type="text"
+                  className="form-control ml-3"
+                  placeholder="Auteur"
+                  required
+                  onChange={(e) =>
+                    setnewData({ ...newData, author: e.target.value })
+                  }
+                />
+              </div>
+              <div className="form-group">
+                <button className="btn btn-outline-secondary ml-3">
+                  Ajouter un livre
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="container" style={{ minHeight: "200px" }}>
+          <div className="row">
+            <div className="col-md-12">
+              <ul className="list-group">
+                <li className="list-group-item list-group-item-light d-flex justify-content-between">
+                  livres enregitrés ici
+                </li>
+              </ul>
+              <div className="d-flex justify-content-center">
+                <button className="btn btn-danger mt-4 mb-5">
+                  Effacer tous les livres
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
+
+  export default AddBooks
